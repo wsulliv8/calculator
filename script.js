cc2798a70fa8451ab96238b52e0d4119ff899177
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2024-10-18 18:54:28 -0700

    Remove unnecessary acceptInput function and create update display function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let log = console.log;
+let buttons = document.querySelector('.buttons');
+let display = document.querySelectorAll('.output');
+let buttonHash = createHash(buttons);
+let input = '';
+
+buttons.addEventListener('click', (event) => {
+  let button = event.target.innerText;
+  input = changeInput(button, input);
+  displayInput(input);
+  return;
+});
+document.addEventListener('keydown', (event) => {
+  let button = event.key;
+  input = changeInput(button, input);
+  log(input);
+  displayInput(input);
+  return;
+});
 
 function Calculator() {
   this.methods = {
@@ -20,36 +38,33 @@ function Calculator() {
   };
 }
 
-function acceptInput() {
-  let input = '';
-  let buttons = document.querySelector('.buttons');
-
-  buttons.addEventListener('click', (event) => {
-    let button = event.target.innerText;
-    input = changeInput(button, input);
-    log(input);
-  });
-  //need to check if button is allowed via a dictionary
-  document.addEventListener('keydown', (event) => {
-    let button = event.key;
-    input = changeInput(button, input);
-    log(input);
-  });
-  if (typeof input === 'number') {
-    log(input);
-    return input;}
-}
-
 function changeInput(button, input){
   switch (button) {
     case 'Enter':
     case '=':
       let calculator = new Calculator();
-      return calculator.calculate(input);
-      break;
-    default: 
-      return input += button;
+      return calculator.calculate(input).toString();
+    default:
+      input = (button in buttonHash) ? input+=button : input;
+      return input;
   }
 }
 
-acceptInput();
+function createHash(element) {
+  let nodeList = element.children;
+  let hash = {}
+  for (const node of nodeList) {
+    hash[node.innerText] = true;
+  }
+  return hash;
+}
+
+function displayInput(input) {
+  display.forEach((cell) => cell.innerText = '');
+
+  for (let i=(input.length-1), j=0; i>=0; i--, j++) {
+    display[i].innerText = input[j];
+  }
+}
+
+
