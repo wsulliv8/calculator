ed6164a4a56cb78b0567a70b8b0d33fec4c77ff4
wsulliv8 <35410714+wsulliv8@users.noreply.github.com>
2024-10-19 15:15:01 -0700

    Add innerDisplay resize and increase font size for output

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let log = console.log;
 let buttons = document.querySelector('.buttons');
 let display = document.querySelectorAll('.output');
+let displayInner = document.querySelector('.innerDisplay');
 let buttonHash = createHash(buttons);
 let input = '';
 let complete = false;
 let calculator = new Calculator();
+let resizeTimer;
 
 buttons.addEventListener('click', (event) => handleInput(event.target.textContent));
 document.addEventListener('keydown', (event) => handleInput(event.key));
+//Change # of output blocks based on screen size
+
+window.addEventListener('resize', resizeDisplay);
+resizeDisplay();
+//window.addEventListener('resize', () => log('resize!'));
 
 function handleInput(button) {
   switch (button) {
@@ -57,33 +64,30 @@ function Calculator() {
     '+/-': (x) => x*(-1),
   }
 /* The code snippet below was my attempt at implementing the eval
-function. Handling order of operations got too complex, so I opted
-to use the eval function */
-/* 
+function. */
   this.calculate = (str) => {
     //create arrays for operators and operands
     let operators = str.match(/[^\.\d]/g);
     let operands = str.split(/[^\.\d]/).map((char) => +char);
     /*ACTION: Able to accept multiple inputs (includind decimals)
     but does not observe proper order of operations. Need to build
-    parser for handling this. 
+    parser for handling this.*/
     return operands.reduce( (total,current,index, operands) => {
       if (index === operands.length)
         return total;
       else
         return this.methods[operators[index-1]](total,operands[index]);
-
     })
   };
-*/
 
-  this.calculate = (str) => Function("return " + str)();
+//  this.calculate = (str) => Function("return " + str)();
 
   this.calculateSpecial = (x, op) => this.methods[op](x);
 }
 
 function createHash(element) {
   let nodeList = element.children;
+
   let hash = {}
   for (const node of nodeList) {
     hash[node.innerText] = true;
@@ -103,4 +107,24 @@ function clearDisplay() {
   display.forEach((cell) => cell.innerText = '');
 }
 
+function resizeDisplay() {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout( () => {
+    displayInner = document.querySelector('.innerDisplay');
+    let desired = Math.floor(displayInner.offsetWidth/18);
+    let actual = displayInner.children.length;
+    while (actual > desired) {
+      displayInner.removeChild(displayInner.lastElementChild);
+      actual--;
+    }
+    while (actual < desired) {
+      const output = document.createElement('div');
+      output.classList.add('output');
+      displayInner.appendChild(output);
+      actual++;
+    }
+    display = document.querySelectorAll('.output');
+  }, 300);
+  return;
+}
 
